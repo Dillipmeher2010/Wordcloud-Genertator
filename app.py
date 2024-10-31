602f006832626b30b5692f230e718cd865a886f2
DILLIP MEHER2010 <dillipmeher2010@gmail.com>
2024-10-31 13:43:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from PIL import Image
+import io
 
 # Set the title of the app
 st.title("Word Cloud Generator")
@@ -19,8 +21,21 @@ if st.button("Generate Word Cloud"):
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis('off')  # Turn off axis
         plt.tight_layout()
-        
+
         # Show the word cloud in Streamlit
         st.pyplot(plt)
+
+        # Save the word cloud to a BytesIO object
+        img_buffer = io.BytesIO()
+        wordcloud.to_image().save(img_buffer, format='PNG')
+        img_buffer.seek(0)
+
+        # Provide a download button for the generated word cloud
+        st.download_button(
+            label="Download Word Cloud",
+            data=img_buffer,
+            file_name="wordcloud.png",
+            mime="image/png"
+        )
     else:
         st.warning("Please enter some text to generate a word cloud.")
